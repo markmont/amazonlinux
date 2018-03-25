17794ae5fdc5222197f0b59122c09210911557c5
Wanpeng Li <wanpengli@tencent.com>
2018-03-24 21:17:24 -0700

    KVM: X86: Fix setup the virt_spin_lock_key before static key get initialized
     static_key_disable_cpuslocked(): static key 'virt_spin_lock_key+0x0/0x20' used before call to jump_label_init()
     WARNING: CPU: 0 PID: 0 at kernel/jump_label.c:161 static_key_disable_cpuslocked+0x61/0x80
     RIP: 0010:static_key_disable_cpuslocked+0x61/0x80
     Call Trace:
      static_key_disable+0x16/0x20
      start_kernel+0x192/0x4b3
      secondary_startup_64+0xa5/0xb0
    
    Qspinlock will be choosed when dedicated pCPUs are available, however, the
    static virt_spin_lock_key is set in kvm_spinlock_init() before jump_label_init()
    has been called, which will result in a WARN(). This patch fixes it by delaying
    the virt_spin_lock_key setup to .smp_prepare_cpus().
    
    Reported-by: Davidlohr Bueso <dbueso@suse.de>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Radim Krčmář <rkrcmar@redhat.com>
    Cc: Davidlohr Bueso <dbueso@suse.de>
    Signed-off-by: Wanpeng Li <wanpengli@tencent.com>
    Fixes: b2798ba0b876 ("KVM: X86: Choose qspinlock when dedicated physical CPUs are available")
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>
    (cherry picked from commit 34226b6b70980a8f81fff3c09a2c889f77edeeff)
    Reviewed-by: Vallish Vaidyeshwara <vallish@amazon.com>
    Reviewed-by: Anchal Agarwal <anchalag@amzon.com>
    Signed-off-by: Eduardo Valentin <eduval@amazon.com>

--- arch/x86/kernel/kvm.c
+++ arch/x86/kernel/kvm.c
@@ -450,6 +450,13 @@ static int kvm_cpu_online(unsigned int cpu)
 }
 
 #ifdef CONFIG_SMP
+static void __init kvm_smp_prepare_cpus(unsigned int max_cpus)
+{
+	native_smp_prepare_cpus(max_cpus);
+	if (kvm_para_has_hint(KVM_HINTS_DEDICATED))
+		static_branch_disable(&virt_spin_lock_key);
+}
+
 static void __init kvm_smp_prepare_boot_cpu(void)
 {
 	kvm_guest_cpu_init();
@@ -531,6 +538,7 @@ void __init kvm_guest_init(void)
 		kvm_setup_vsyscall_timeinfo();
 
 #ifdef CONFIG_SMP
+	smp_ops.smp_prepare_cpus = kvm_smp_prepare_cpus;
 	smp_ops.smp_prepare_boot_cpu = kvm_smp_prepare_boot_cpu;
 	if (cpuhp_setup_state_nocalls(CPUHP_AP_ONLINE_DYN, "x86/kvm:online",
 				      kvm_cpu_online, kvm_cpu_down_prepare) < 0)
@@ -698,10 +706,8 @@ void __init kvm_spinlock_init(void)
 	if (!kvm_para_has_feature(KVM_FEATURE_PV_UNHALT))
 		return;
 
-	if (kvm_para_has_hint(KVM_HINTS_DEDICATED)) {
-		static_branch_disable(&virt_spin_lock_key);
+	if (kvm_para_has_hint(KVM_HINTS_DEDICATED))
 		return;
-	}
 
 	__pv_init_lock_hash();
 	pv_lock_ops.queued_spin_lock_slowpath = __pv_queued_spin_lock_slowpath;

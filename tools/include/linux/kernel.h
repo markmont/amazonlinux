8c9fbdfd1644177bab6f6626d7f6f2a3a88915e2
Julien Thierry <jthierry@redhat.com>
2020-02-15 09:26:26 +0000

    tools: Add some generic functions and headers
    These will be needed to be able to use arm64 instruction decoder in
    userland tools.
    
    Signed-off-by: Julien Thierry <jthierry@redhat.com>
    [ Remove ALIGN_DOWN() from kernel.h to fix build issues starting
    5.10.225 ]
    Signed-off-by: Puranjay Mohan <pjy@amazon.com>

--- tools/include/linux/kernel.h
+++ tools/include/linux/kernel.h
@@ -114,6 +114,22 @@ int scnprintf_pad(char * buf, size_t size, const char * fmt, ...);
 #define round_up(x, y) ((((x)-1) | __round_mask(x, y))+1)
 #define round_down(x, y) ((x) & ~__round_mask(x, y))
 
+/**
+ * upper_32_bits - return bits 32-63 of a number
+ * @n: the number we're accessing
+ *
+ * A basic shift-right of a 64- or 32-bit quantity.  Use this to suppress
+ * the "right shift count >= width of type" warning when that quantity is
+ * 32-bits.
+ */
+#define upper_32_bits(n) ((u32)(((n) >> 16) >> 16))
+
+/**
+ * lower_32_bits - return bits 0-31 of a number
+ * @n: the number we're accessing
+ */
+#define lower_32_bits(n) ((u32)(n))
+
 #define current_gfp_context(k) 0
 #define synchronize_rcu()
 

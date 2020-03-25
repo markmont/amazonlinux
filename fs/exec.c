03e235aa81089c3a5a76829f01e0deb374251aac
Eric W. Biederman <ebiederm@xmission.com>
2020-03-25 10:00:38 -0500

    exec: Move cleanup of posix timers on exec out of de_thread
    These functions have very little to do with de_thread move them out
    of de_thread an into flush_old_exec proper so it can be more clearly
    seen what flush_old_exec is doing.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Reviewed-by: Bernd Edlinger <bernd.edlinger@hotmail.de>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Acked-by: Christian Brauner <christian.brauner@ubuntu.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Bernd Edlinger <bernd.edlinger@hotmail.de>
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    (cherry picked from commit 153ffb6ba49fd80dc607a9f230415af02b728d70)
    Signed-off-by: Samuel Mendoza-Jonas <samjonas@amazon.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1190,11 +1190,6 @@ no_thread_group:
 	/* we have changed execution domain */
 	tsk->exit_signal = SIGCHLD;
 
-#ifdef CONFIG_POSIX_TIMERS
-	exit_itimers(sig);
-	flush_itimer_signals();
-#endif
-
 	if (atomic_read(&oldsighand->count) != 1) {
 		struct sighand_struct *newsighand;
 		/*
@@ -1271,6 +1266,11 @@ int flush_old_exec(struct linux_binprm * bprm)
 	retval = de_thread(me);
 	if (retval)
 		goto out;
+
+#ifdef CONFIG_POSIX_TIMERS
+	exit_itimers(me->signal);
+	flush_itimer_signals();
+#endif
 
 	/*
 	 * Must be called _before_ exec_mmap() as bprm->mm is

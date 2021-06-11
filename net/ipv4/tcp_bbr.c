8b73b3206fac0cd74388a6a9211b2616e0db654c
Adithya Abraham Philip <abrahamphilip@google.com>
2021-06-11 21:56:10 +0000

    net-tcp_bbr: v3: ensure ECN-enabled BBR flows set ECT on retransmits
    Adds a new flag TCP_ECN_ECT_PERMANENT that is used by CCAs to
    indicate that retransmitted packets and pure ACKs must have the
    ECT bit set. This is necessary for BBR, which when using
    ECN expects ECT to be set even on retransmitted packets and ACKs.
    
    Previous to this addition of TCP_ECN_ECT_PERMANENT, CCAs which can use
    ECN but don't "need" it did not have a way to indicate that ECT should
    be set on retransmissions/ACKs.
    
    Signed-off-by: Adithya Abraham Philip <abrahamphilip@google.com>
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    Change-Id: I8b048eaab35e136fe6501ef6cd89fd9faa15e6d2
    Signed-off-by: Samuel Mendoza-Jonas <samjonas@amazon.com>

--- net/ipv4/tcp_bbr.c
+++ net/ipv4/tcp_bbr.c
@@ -2129,6 +2129,9 @@ static void bbr_init(struct sock *sk)
 	bbr->alpha_last_delivered_ce = 0;
 
 	tp->fast_ack_mode = bbr_fast_ack_mode ? 1 : 0;
+
+	if (bbr_can_use_ecn(sk))
+		tp->ecn_flags |= TCP_ECN_ECT_PERMANENT;
 }
 
 /* BBR marks the current round trip as a loss round. */

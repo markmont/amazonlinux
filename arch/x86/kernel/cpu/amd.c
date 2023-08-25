b920511303321144ebdeca2bc511e12ed42dcecd
Kuniyuki Iwashima <kuniyu@amazon.com>
2023-08-25 21:29:42 +0000

    x86/CPU/AMD: Do not leak quotient data after a division by 0.
    commit 77245f1c3c6495521f6a3af082696ee2f8ce3921 upstream.
    commit f58d6fbcb7c848b7f2469be339bc571f2e9d245b upstream.
    
    This patch is a joint commit of these two:
    
      * 77245f1c3c64 ("x86/CPU/AMD: Do not leak quotient data after a division by 0")
      * f58d6fbcb7c8 ("x86/CPU/AMD: Fix the DIV(0) initial fix attempt")
    
    In the original fix, we add mocked divisions in two places.
    
      * arch_exit_to_user_mode()
      * svm_vcpu_enter_exit()
    
    The former corresponds to prepare_exit_to_usermode() in 4.14, and
    the latter is svm_vcpu_run().
    
    In arch_exit_to_user_mode(), we add the mocked division just
    after mds_user_clear_cpu_buffers(), which was originally placed
    in prepare_exit_to_usermode() of 4.14 kernel.
    
      prepare_exit_to_usermode
      `-> exit_to_user_mode
          `-> arch_exit_to_user_mode
    
    We can follow the move in these two commits.
    
      * 8f159f1dfa1e ("x86/entry/common: Protect against instrumentation")
      * 167fd210ec05 ("x86/entry: Use generic syscall exit functionality")
    
    So, we add the division in prepare_exit_to_usermode().
    
    Also, svm_vcpu_enter_exit() was split from svm_vcpu_run() by
    commit 135961e0a7d5 ("x86/kvm/svm: Move guest enter/exit into
    .noinstr.text").  We can add the division before entering the
    guest mode.
    
    Other notable thing is that we remove noinstr from amd_clear_divider()
    as noinstr was introduced after 4.14 by commit 655389666643
    ("vmlinux.lds.h: Create section for protection against instrumentation").
    
    === Original commit message from here ===
    === (1/2)
    
    x86/CPU/AMD: Do not leak quotient data after a division by 0
    
    Under certain circumstances, an integer division by 0 which faults, can
    leave stale quotient data from a previous division operation on Zen1
    microarchitectures.
    
    Do a dummy division 0/1 before returning from the #DE exception handler
    in order to avoid any leaks of potentially sensitive data.
    
    === (2/2)
    
    x86/CPU/AMD: Fix the DIV(0) initial fix attempt
    
    Initially, it was thought that doing an innocuous division in the #DE
    handler would take care to prevent any leaking of old data from the
    divider but by the time the fault is raised, the speculation has already
    advanced too far and such data could already have been used by younger
    operations.
    
    Therefore, do the innocuous division on every exit to userspace so that
    userspace doesn't see any potentially old data from integer divisions in
    kernel space.
    
    Do the same before VMRUN too, to protect host data from leaking into the
    guest too.
    
    ===
    
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -79,6 +79,10 @@ static const int amd_erratum_1485[] =
 	AMD_LEGACY_ERRATUM(AMD_MODEL_RANGE(0x19, 0x10, 0x0, 0x1f, 0xf),
 			   AMD_MODEL_RANGE(0x19, 0x60, 0x0, 0xaf, 0xf));
 
+static const int amd_div0[] =
+	AMD_LEGACY_ERRATUM(AMD_MODEL_RANGE(0x17, 0x00, 0x0, 0x2f, 0xf),
+			   AMD_MODEL_RANGE(0x17, 0x50, 0x0, 0x5f, 0xf));
+
 static bool cpu_has_amd_erratum(struct cpuinfo_x86 *cpu, const int *erratum)
 {
 	int osvw_id = *erratum++;
@@ -1127,6 +1131,11 @@ static void init_amd(struct cpuinfo_x86 *c)
 	if (!cpu_has(c, X86_FEATURE_HYPERVISOR) &&
 	     cpu_has_amd_erratum(c, amd_erratum_1485))
 		msr_set_bit(MSR_ZEN4_BP_CFG, MSR_ZEN4_BP_CFG_SHARED_BTB_FIX_BIT);
+
+	if (cpu_has_amd_erratum(c, amd_div0)) {
+		pr_notice_once("AMD Zen1 DIV0 bug detected. Disable SMT for full protection.\n");
+		setup_force_cpu_bug(X86_BUG_DIV0);
+	}
 }
 
 #ifdef CONFIG_X86_32
@@ -1255,3 +1264,14 @@ void amd_check_microcode(void)
 
 	on_each_cpu(zenbleed_check_cpu, NULL, 1);
 }
+
+/*
+ * Issue a DIV 0/1 insn to clear any division data from previous DIV
+ * operations.
+ */
+void amd_clear_divider(void)
+{
+	asm volatile(ALTERNATIVE("", "div %2\n\t", X86_BUG_DIV0)
+		     :: "a" (0), "d" (0), "r" (1));
+}
+EXPORT_SYMBOL_GPL(amd_clear_divider);
